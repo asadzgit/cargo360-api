f5fdf6fb91df7fef29efa69c222624e20af3b315
Muhammad Asad <asadmahmood41999@gmail.com>
2025-11-15 06:19:44 +0500

    increased jwt life

--- config/env.js
+++ config/env.js
@@ -13,8 +13,8 @@ module.exports = {
   jwt: {
     accessSecret: process.env.JWT_ACCESS_SECRET,
     refreshSecret: process.env.JWT_REFRESH_SECRET,
-    accessExpires: process.env.JWT_ACCESS_EXPIRES || '15m',
-    refreshExpires: process.env.JWT_REFRESH_EXPIRES || '7d'
+    accessExpires: process.env.JWT_ACCESS_EXPIRES || '30d',
+    refreshExpires: process.env.JWT_REFRESH_EXPIRES || '40d'
   },
   corsOrigin: process.env.CORS_ORIGIN || '*'
 };
